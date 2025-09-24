fdb2953dc3f10cc4874002639d3c03991e0bfaf9
tuihblsr <vtyan5291@gmail.com>
2025-09-23 20:10:04 -0400

    Demand system!

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ let player = {
 
 function buyIngredient(ingredient) {
     if (player.money >= player.ingredients[ingredient].price) {
-        player.money -= player.ingredients[ingredient].price;
-        player.ingredients[ingredient].amount += player.ingredients[ingredient].purchaseAmount;
-        console.log(`Bought ${player.ingredients[ingredient].purchaseAmount} ${ingredient}.`);
+        player.money -= player.ingredients[ingredient].price
+        player.ingredients[ingredient].amount += player.ingredients[ingredient].purchaseAmount
+        console.log(`Bought ${player.ingredients[ingredient].purchaseAmount} ${ingredient}.`)
     } else {
         console.log("Not enough money")
     }
@@ -25,93 +25,112 @@ function buyIngredient(ingredient) {
 function haveEnough(required, available) {
     for (let item in required) {
         if (required[item] > available[item].amount) {
-            return false;
+            return false
         }
     }
-    return true;
+    return true
 }
 
 function renderUI() {
-    document.querySelector("p span").innerText = player.money;
-    // document.getElementById("brewTea").style.display = "";
+    document.querySelector("p span").innerText = player.money
+    // document.getElementById("brewTea").style.display = ""
 
     //warehouse
     for (let ingredient in player.ingredients) {
         if (document.getElementById(`${ingredient}Row`).checkVisibility()) {
-            document.getElementById(`${ingredient}Stock`).innerText = player.ingredients[ingredient].amount;
-            document.getElementById(`${ingredient}Buy`).innerText = `+${player.ingredients[ingredient].purchaseAmount} ($${player.ingredients[ingredient].price})`;
+            document.getElementById(`${ingredient}Stock`).innerText = player.ingredients[ingredient].amount
+            document.getElementById(`${ingredient}Buy`).innerText = `+${player.ingredients[ingredient].purchaseAmount} ($${player.ingredients[ingredient].price})`
         }
     }
 
     //market
-    document.getElementById("marketSection").style.display = player.stage >= 1 ? "" : "none";
+    document.getElementById("marketSection").style.display = player.stage >= 1 ? "" : "none"
     for (let tea in player.brewedTea) {
         if (document.getElementById(`${tea}Row`).checkVisibility()) {
-            document.getElementById(`${tea}Stock`).innerText = player.brewedTea[tea].amount;
-            document.getElementById(`${tea}Price`).innerText = player.brewedTea[tea].price;
-            document.getElementById(`${tea}Demand`).innerText = player.brewedTea[tea].demand;
+            document.getElementById(`${tea}Stock`).innerText = player.brewedTea[tea].amount
+            document.getElementById(`${tea}Price`).innerText = player.brewedTea[tea].price
+            document.getElementById(`${tea}Demand`).innerText = player.brewedTea[tea].demand
         }
     }
 
     //visibility
-    // document.getElementById("warehouseSection").style.display = "";
-    document.getElementById("sweetenersRow").style.display = player.ingredients.sweeteners > 0 ? "" : "none";
+    // document.getElementById("warehouseSection").style.display = ""
+    document.getElementById("sweetenersRow").style.display = player.ingredients.sweeteners > 0 ? "" : "none"
 }
 
-function calculateDemand(popularity) {
-    const thresholds = [ // popularity, demand per second
-        [50, 0.1],
-        [100, 0.15],
-        [200, 0.2],
-        [600, 0.3],
-        [1500, 0.4],
-    ]
-    let thresholdToDemand = thresholds.map(([max, perSecond], index) => [max, index === 0 ? 0 : thresholds[index - 1][0], value])
-    let demand = 0;
-    if (popularity === 0) demand++; //if it's somehow zero then do nothing
-    
-}
-
-function calculateDemandTemp() {
-    for (let tea in player.brewedTea) {
-        if (player.brewedTea[tea].demand < 10) {
-            player.brewedTea[tea].demand++;
+const thresholds = [ // popularity, demand per second
+    [50, 0.25],
+    [100, 0.255],
+    [200, 0.3],
+    [600, 0.4],
+    [1500, 0.5],
+]
+const thresholdToDemand = thresholds.map(([max, perSecond], index) => [max, index === 0 ? 0 : thresholds[index - 1][0], perSecond])
+// 0 - max, 1 - min, 2 - demand per second
+let thresholdIndex = 0
+let timeAccumulated = 0
+function calculateDemand(popularity, delta) {
+    let isMaxed = thresholdIndex === thresholdToDemand.length
+    let currentMax = null
+    if (!isMaxed) {
+        currentMax = thresholdToDemand[thresholdIndex][0]
+        if (popularity < thresholdToDemand[thresholdIndex][1]) { //if you got too far ahead reset back
+            thresholdIndex = 0
+            currentMax = thresholdToDemand[thresholdIndex][0]
         }
-        if (player.brewedTea[tea].amount >= 1 && player.brewedTea[tea].demand >= 1) {
-            player.brewedTea[tea].amount--; player.brewedTea[tea].demand--;
-            player.money += player.brewedTea[tea].price;
+
+        while (popularity > currentMax) {
+            thresholdIndex++
+            isMaxed = thresholdIndex === thresholdToDemand.length
+            if (isMaxed) break //if we run out of ranges, break
+            currentMax = thresholdToDemand[thresholdIndex][0]
+        }
+    }
+    const demandPerSecond = isMaxed ? 20 + Math.floor(popularity / 100000) * 10 : thresholdToDemand[thresholdIndex][2] //if it's maxed then just add 10 for every 100k
+    const demandCap = demandPerSecond * 60 //cap to 1 minute max
+    const timeUntilOrder = 1 / demandPerSecond //this is in seconds!!
+    timeAccumulated += delta
+    const newOrders = Math.floor(timeAccumulated / timeUntilOrder)
+    timeAccumulated -= timeUntilOrder * newOrders
+    player.brewedTea.blandTea.demand += newOrders
+}
+
+function sellTea() {
+    for (let tea in player.brewedTea) {
+        if (player.brewedTea[tea].demand > 0) {
+            player.brewedTea[tea].amount--; player.brewedTea[tea].demand--; player.money += player.brewedTea[tea].price
         }
     }
 }
 
 function updateGame(deltaTime) {
-    calculateDemandTemp();
-    renderUI();
+    if (player.stage > 0) calculateDemand(player.popularity, deltaTime)
+    renderUI()
 }
 
 window.onload = () => {
-    const brewTea = document.getElementById("brewTea");
+    const brewTea = document.getElementById("brewTea")
     brewTea.addEventListener("click", () => {
         if (player.stage === 0) {
-            player.stage++;
-            console.log(`player.stage = ${player.stage}`);
+            player.stage++
+            console.log(`player.stage = ${player.stage}`)
         }
-        const required = player.brewedTea.blandTea.creation;
+        const required = player.brewedTea.blandTea.creation
         if (haveEnough(required, player.ingredients)) {
             for (let item in required) {
-                player.ingredients[item].amount -= required[item];
+                player.ingredients[item].amount -= required[item]
             }
-            player.brewedTea.blandTea.amount += 1;
-            console.log("Brewed a cup of bland tea!");
+            player.brewedTea.blandTea.amount++
+            console.log("Brewed a cup of bland tea!")
         } else {
-            console.log("Not enough ingredients to brew tea.");
+            console.log("Not enough ingredients to brew tea.")
         }
     })
-    let lastTime = performance.now();
+    let lastTime = performance.now()
     setInterval(function gameLoop() {
-        const currentTime = performance.now();
-        const deltaTime = (currentTime - lastTime) / 1000;
-        lastTime = currentTime;
-        updateGame(deltaTime);
-    });
+        const currentTime = performance.now()
+        const deltaTime = (currentTime - lastTime) / 1000
+        lastTime = currentTime
+        updateGame(deltaTime)
+    })
 }
